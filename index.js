293fbd29fa65b11c2f16c03483985fe5a5097b30
pandey-anshika <paanshika2203@gmail.com>
2023-07-27 17:34:07 +0530

    next commit

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const config = require('config');
 const Joi = require('joi');
+const multer = require('multer');
 const { default: mongoose } = require('mongoose');
 const app = express();
 const blogs = require('./routes/blogs');
 const user = require('./routes/user');
 const {mw} = require('./middleware');
+const login = require('./routes/login');
 
 mongoose.connect('mongodb://127.0.0.1:27017/newwprod')
 .then((res)=> console.log('connected to mongodb::'))
@@ -15,6 +17,30 @@ app.use(express.json());
 app.use('/api/blogs',blogs);
 app.use('/api/user',user);
 app.use(mw);
+app.use('/api/login', login);
+
+
+var storage = multer.diskStorage({
+
+    destination: function (req, file, cb) {
+ 
+       cb(null, 'uploads');
+     },
+     filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname);
+     }
+  });
+  var upload = multer({ storage: storage });
+ 
+  app.post('/api/upload', upload.single('image'),(req, res) => {
+    const image = req.image;
+    // res.send(image);
+      res.send(apiResponse({message: 'File uploaded successfully.', image}));
+  });
+ 
+  function apiResponse(results){
+      return JSON.stringify({"status": 200, "response": results});
+  }
 
 const port = process.env.PORT || 3000;
 app.listen(port, ()=>{console.log('listening...'); });
